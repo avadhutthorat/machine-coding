dd02093d7fe27e6be405abfb4ba20d9b71609562
Avadhut Thorat <avadhutthorat40@gmail.com>
2025-09-02 18:51:56 +0530

    Top K Frequent numbers

---
+++ heap/top-k-frequent-numbers.py
@@ -0,0 +1,21 @@
+import heapq
+
+def topKFrequentNumbers(arr, k):
+    dict = {}
+    result = []
+
+    print(dict.get('name') or 0 + 1)
+
+    for _, num in enumerate(arr):
+        value = dict[num] + 1 if num in dict else 1
+        dict[num] = value
+
+    for _, num in enumerate(dict):
+        print(dict[num], num)
+        heapq.heappush(result, (dict[num], num) )
+        if len(result) > k:
+            heapq.heappop(result)
+    result = [t[1] for t in result]
+    return result        
+
+print(topKFrequentNumbers([1,1,1,3,2,2,2,4,4, 4], 2))
